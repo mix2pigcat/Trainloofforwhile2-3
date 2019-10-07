1268690a6caf71f23535b056008e605e314fd3e6
thanhle <mix2pigcat@gmaail.com>
2019-10-07 12:00:59 +0700

    filebai

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// // for(var i =1;i<=5;i++){
+// //     for (var j=1;j<=i;j++){
+// //         document.write('*')
+// //     } document.write("<br/>")
+// // }
+//
+// for(var i=5;i>=1;i--){
+//     for(var j=1;j<=i;j++){
+//         document.write('*');
+//     } document.write("<br/>")
+// }
+//
+// // for(var i =1;i<=5;i++){
+// //     for (var j=5;j>=i;j--){
+// //         // if(i==j){
+// //             document.write('*');
+// //         // }
+// //     }document.write("<br/>")
+// // }
